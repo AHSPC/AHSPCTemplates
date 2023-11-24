2a0e3b03c229449882563fbdffb8df8fcf1f93f5
blobbybilb <arjun.albany@gmail.com>
2023-11-24 12:06:00 -0800

    Update ai.js

--- AI/ai.js
+++ AI/ai.js
@@ -1,29 +1,3 @@
-const apiUrl = "http://64.225.89.231:11434/api/generate";
-
 async function askAI(prompt) {
-    const data = {
-        model: "mistral",
-        prompt: prompt,
-        stream: false
-    };
-
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const responseData = await response.json();
-        return responseData.response;
-    } catch (error) {
-        console.error("Error:", error.message);
-        return null;
-    }
+  return await (await fetch('https://ahspc.deno.dev/ai', { method: 'POST', body: prompt })).text()
 }
